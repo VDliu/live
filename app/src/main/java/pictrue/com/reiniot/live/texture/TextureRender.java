f94c44a88731308315e49900ba35c5ec148320cc
liuge <756384955@qq.com>
2019-08-06 17:30:52 +0800

    添加vbo

--- app/src/main/java/pictrue/com/reiniot/live/texture/TextureRender.java
+++ app/src/main/java/pictrue/com/reiniot/live/texture/TextureRender.java
@@ -48,6 +48,13 @@ public class TextureRender implements LgGlSurfaceView.LGRender {
     private int textureid;
     private int sampler;
 
+    private int vboId;
+
+    /**
+     * 不使用VBO时，我们每次绘制（ glDrawArrays ）图形时都是从本地内存处获取顶点数据然后传输给OpenGL来绘制，这样就会频繁的操作CPU->GPU增大开销，从而降低效率。
+     * 使用VBO，我们就能把顶点数据缓存到GPU开辟的一段内存中，然后使用时不必再从本地获取，而是直接从显存中获取，这样就能提升绘制的效率。
+     */
+
 
     public TextureRender(Context context) {
         mContext = context;
@@ -76,6 +83,27 @@ public class TextureRender implements LgGlSurfaceView.LGRender {
         fPosition = GLES20.glGetAttribLocation(program, "f_Position");
         sampler = GLES20.glGetUniformLocation(program, "sTexture");
 
+        int[] vbos = new int[1];
+        //在显卡上创建vbo
+        GLES20.glGenBuffers(1, vbos, 0);
+        vboId = vbos[0];
+
+
+        //绑定
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+
+        //开辟vbo空间 传null表示先不放如数据
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4 + fragmentData.length * 4, null, GLES20.GL_STATIC_DRAW);
+
+        //设置顶点数据,先传入顶点坐标
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, 0, vertexData.length * 4, vertexBuffer);
+
+        //设置顶点数据,传入纹理坐标
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4, fragmentData.length * 4, fragmentBuffer);
+
+        //解绑
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+
         int[] textureId = new int[1];
         //在显卡上开辟了一张纹理
         GLES20.glGenTextures(1, textureId, 0);
@@ -103,7 +131,6 @@ public class TextureRender implements LgGlSurfaceView.LGRender {
     @Override
     public void onSurfaceChanged(int width, int height) {
         GLES20.glViewport(0, 0, width, height);
-
     }
 
     @Override
@@ -114,17 +141,25 @@ public class TextureRender implements LgGlSurfaceView.LGRender {
         GLES20.glUseProgram(program);
         //绑定纹理
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureid);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
         //使能顶点坐标
         GLES20.glEnableVertexAttribArray(vPosition);
-        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, vertexBuffer);
+        //直接在cpu vertexBuffer中取出顶点坐标
+        //GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, vertexBuffer);
+        //在gpu vbo buffer中去取出顶点坐标
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, 0);
 
         //使能纹理坐标
         GLES20.glEnableVertexAttribArray(fPosition);
-        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, fragmentBuffer);
+        //直接从cpu fragmentBuffer中获取到纹理坐标
+        //GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, fragmentBuffer);
+        //从gpu vbo中的buffer中获取到纹理坐标
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, vertexData.length * 4);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
         //解绑纹理
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
 
     }
 }
